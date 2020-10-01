8d72193ca09958d9016acf201173fc44036d86a1
Anders Ekman <oakismen@gmail.com>
2020-10-01 16:22:27 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const apiKey = "ifqUIAvwExByDEA0NLbqEXN2w8vSef2dQovE";
+  const alley = {
+    companyName: "Strike & Co Göteborg",
+    country: "Sweden",
+    countryCode: "se",
+    lanes: 28,
+    system: "vbs_viking",
+    uuid: "cdab209b-f1f2-11e6-b920-0050569337ac",
+  };
+  const fromLane = 14;
+  const toLane = 21;
+  const numberOfLanes = Math.abs(fromLane - toLane);
+  let selectedLanes = [];
+
+  for (let index = 0; index <= numberOfLanes; index++) {
+    selectedLanes.push(fromLane + index);
+  }
+
+  console.log(selectedLanes);
+
+  return (
+    <div className="App">
+      <div className="lane-wrapper">
+        {selectedLanes.map((lane) => (
+          <img
+            className="lane"
+            alt={lane}
+            src={`https://scoring.lanetalk.com/upload/${alley.uuid}/VTVFile${lane}.jpg`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// Hämta lista med alla bowlinghallar
+// Använd dropdowns för att välja hall och banor
+// Klicka på knapp för att få länk till overlay för vald hall och banor
