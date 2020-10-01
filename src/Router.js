724b184f9453046131f93bdc440cd54bfcd7cc4d
Anders Ekman <oakismen@gmail.com>
2020-10-01 22:26:30 +0200

    use hashrouter

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import SetupView from "./views/SetupView";
 import OverlayView from "./views/OverlayView";
-
-const nameSpace = "sterling-scoring";
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route path={`/${nameSpace}/:alley/:fromLane/:toLane`}>
-          <OverlayView />
-        </Route>
-        <Route path={`/${nameSpace}`}>
-          <SetupView />
-        </Route>
-      </Switch>
+      <Route path="/:alley/:fromLane/:toLane" component={OverlayView} />
+      <Route exact path="/" component={SetupView} />
     </Router>
   );
 }
