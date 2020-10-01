7449da125a4a3d2144987fefa7e5aebc0c89a2f7
Anders Ekman <oakismen@gmail.com>
2020-10-01 16:37:41 +0200

    reload every 5 s

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  setTimeout(() => {
+    window.location.reload();
+  }, 5000);
+
   const apiKey = "ifqUIAvwExByDEA0NLbqEXN2w8vSef2dQovE";
+
   const alley = {
     companyName: "Strike & Co Göteborg",
     country: "Sweden",
@@ -11,22 +16,22 @@ function App() {
     system: "vbs_viking",
     uuid: "cdab209b-f1f2-11e6-b920-0050569337ac",
   };
+
   const fromLane = 14;
   const toLane = 21;
   const numberOfLanes = Math.abs(fromLane - toLane);
-  let selectedLanes = [];
 
+  let selectedLanes = [];
   for (let index = 0; index <= numberOfLanes; index++) {
     selectedLanes.push(fromLane + index);
   }
-
-  console.log(selectedLanes);
 
   return (
     <div className="App">
       <div className="lane-wrapper">
         {selectedLanes.map((lane) => (
           <img
+            key={lane}
             className="lane"
             alt={lane}
             src={`https://scoring.lanetalk.com/upload/${alley.uuid}/VTVFile${lane}.jpg`}
