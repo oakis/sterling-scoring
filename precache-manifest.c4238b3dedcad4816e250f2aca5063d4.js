fbab04bd302298299bda1921c05212c8780aba82
Anders Ekman <oakismen@gmail.com>
2020-10-01 16:29:18 +0200

    Updates

---
+++ precache-manifest.c4238b3dedcad4816e250f2aca5063d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc80f4e4000d849d72d3fa09a03e47f",
+    "url": "/sterling-scoring/index.html"
+  },
+  {
+    "revision": "17f80c19838f1d288ca5",
+    "url": "/sterling-scoring/static/css/main.f474bda5.chunk.css"
+  },
+  {
+    "revision": "b85eeb03472e2c89879b",
+    "url": "/sterling-scoring/static/js/2.ae81d8d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sterling-scoring/static/js/2.ae81d8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f80c19838f1d288ca5",
+    "url": "/sterling-scoring/static/js/main.4562df55.chunk.js"
+  },
+  {
+    "revision": "c2f0bc2f4f3ee2d74861",
+    "url": "/sterling-scoring/static/js/runtime-main.b0476105.js"
+  }
+]);
